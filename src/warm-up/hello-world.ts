113b182f9d848bd5cc49860a5a3cde68d7ef16b4
Jacob Asper <jacobasper191@gmail.com>
2022-12-20 21:06:39 -0500

    solution

--- src/warm-up/hello-world.ts
+++ src/warm-up/hello-world.ts
@@ -1,7 +1,7 @@
 import { Expect, Equal } from "../../utils/index"
 
 // expected to be string
-type HelloWorld = any
+type HelloWorld = string
 
 // you should make this work
 type test = Expect<Equal<HelloWorld, string>>
