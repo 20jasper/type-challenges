747aaf1f71b35e2e9acd67a99155fdefc355821e
Jacob Asper <jacobasper191@gmail.com>
2022-12-27 22:54:21 -0500

    add explanation

--- src/easy/first-of-array/first-of-array.ts
+++ src/easy/first-of-array/first-of-array.ts
@@ -7,6 +7,15 @@ type First<Type extends any[]> = Type extends [] ?
 	// else return the first element
 	Type[0]
 
+//explanation
+/*
+The Type parameter is already constrained to the arrays of any type, so we don't need to worry about the error cases as long as we don't change the parameters
+
+If we just return Type[0], we will pass every case except those where the array empty, so we can just use a conditional type to return the first element if it exists, else return never
+
+Tags: conditional types, generic constraints, indexed types
+*/
+
 //testcases
 type cases = [
 	Expect<Equal<First<[3, 2, 1]>, 3>>,
