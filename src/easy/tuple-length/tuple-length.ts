1df15eee6424957499b843756ac51d98999500af
Jacob Asper <jacobasper191@gmail.com>
2023-01-01 21:40:08 -0500

    add explanation

--- src/easy/tuple-length/tuple-length.ts
+++ src/easy/tuple-length/tuple-length.ts
@@ -3,6 +3,14 @@ import type { Equal, Expect } from '../../../utils'
 // solution
 type Length<Type extends readonly any[]> = Type["length"]
 
+// explanation
+/*
+To get the length of the tuple, we simply need to access the "length" property on the Type parameter. 
+
+This does pass the first 2 cases, but it does not cause errors where it should for the last 2
+To solve this, we can contrain `Type` to readonly arrays of any type
+*/
+
 // test cases
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
