48bd13ef29e70d478df46e8a01d564970cc7b28c
Jacob Asper <jacobasper191@gmail.com>
2023-01-01 21:56:36 -0500

    add explanation

--- src/easy/exclude/exclude.ts
+++ src/easy/exclude/exclude.ts
@@ -3,6 +3,11 @@ import type { Equal, Expect } from '../../../utils'
 // solution
 type MyExclude<Type, Union> = Type extends Union ? never : Type
 
+// explanation
+/*
+Use a conditional type to add a type if it's in the `Union`. Conditional types are applied to each type in the union type. 
+*/
+
 // testcases
 type cases = [
 	Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
